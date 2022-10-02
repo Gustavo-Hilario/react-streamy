3e30e6078721458de26a921909a5f6c8d1af6851
Gustavo_H <gustavo.hilario@automattic.com>
2022-10-02 15:44:56 -0500

    322. Using Redux Dev Tools to Inspect the Store

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import reducers from "./reducers";
 
 // Implementing Redux into the React application
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDom.render(
     <Provider store={store}>
