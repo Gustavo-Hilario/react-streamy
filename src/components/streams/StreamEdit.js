b8ab4de2bf5a07f9ea8f4c99f20038168533b223
Gustavo_H <gustavo.hilario@automattic.com>
2022-10-05 19:11:43 -0500

    362. Selecting Records from State

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-const StreamList = () => {
+// Get information from react store
+import { connect } from "react-redux";
+
+const StreamList = (props) => {
+    console.log(props);
     return (
         <div>
             <h2>StreamEdit</h2>
@@ -8,4 +12,14 @@ const StreamList = () => {
     );
 };
 
-export default StreamList;
+const mapStateToProps = (
+    state,
+    ownProps /*Props from this specific component */
+) => {
+    console.log(ownProps);
+    return {
+        stream: state.streams[ownProps.match.params.id],
+    };
+};
+
+export default connect(mapStateToProps, {})(StreamList);
