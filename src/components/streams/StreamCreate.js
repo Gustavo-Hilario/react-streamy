153405c64aeb45b3da207c812f62f2e9e7da3dbb
Gustavo_H <gustavo.hilario@automattic.com>
2022-10-06 11:41:36 -0500

    365. Real Code Reuse! – StreamForm

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,36 +1,11 @@
 import React from "react";
 
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
+import StreamForm from "./StreamForm";
 
 import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
-    renderError({ error, touched }) {
-        if (touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            );
-        }
-    }
-
-    // We should change this to a arrow function so THIS can work inside that.
-
-    // META will be responsible for reading errors and others
-    renderInput = (/*formProps*/ { input, label, meta }) => {
-        // console.log(meta);
-        const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-        return (
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input} autoComplete="off" />
-                {this.renderError(meta)}
-            </div>
-        );
-    };
-
     onSubmit = (formValues) => {
         // redux-form helps making sure the code below runs and no event handling would be necessary
         // e.preventDefault();
@@ -44,49 +19,14 @@ class StreamCreate extends React.Component {
 
         return (
             <div>
-                {/* handleSubmit is a function/props that comes from the redux-form library */}
-                <form
-                    className="ui form error"
-                    onSubmit={this.props.handleSubmit(this.onSubmit)}
-                >
-                    <Field
-                        name="title"
-                        component={this.renderInput}
-                        label="Enter Title"
-                    />
-                    <Field
-                        name="description"
-                        component={this.renderInput}
-                        label="Enter Description"
-                    />
-                    <button className="ui button primary">Submit</button>
-                </form>
+                <h3>Create a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} />
             </div>
         );
     }
 }
 
-const validate = (formValues) => {
-    const errors = {};
-
-    if (!formValues.title) {
-        // Errors should have the same name after . as the Field Component. Otherwise redux-form won't be able to correlated both things – errors with their respective field
-        errors.title = "You must enter a title";
-    }
-
-    if (!formValues.description) {
-        errors.description = "You must enter a description";
-    }
-
-    return errors;
-};
-
-const formWrapped = reduxForm({
-    form: "streamCreate",
-    validate,
-})(StreamCreate);
-
 export default connect(
     /*should be mapStateToProps function*/ null,
     { createStream } /*action creator*/
-)(formWrapped);
+)(StreamCreate);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    // We should change this to a arrow function so THIS can work inside that.
+
+    // META will be responsible for reading errors and others
+    renderInput = (/*formProps*/ { input, label, meta }) => {
+        // console.log(meta);
+        const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+        return (
+            <div className={className}>
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+
+    onSubmit = (formValues) => {
+        // redux-form helps making sure the code below runs and no event handling would be necessary
+        // e.preventDefault();
+
+        // console.log(formValues);
+        this.props.onSubmit(formValues);
+    };
+
+    render() {
+        // console.log(this.props);
+
+        return (
+            <div>
+                {/* handleSubmit is a function/props that comes from the redux-form library */}
+                <form
+                    className="ui form error"
+                    onSubmit={this.props.handleSubmit(this.onSubmit)}
+                >
+                    <Field
+                        name="title"
+                        component={this.renderInput}
+                        label="Enter Title"
+                    />
+                    <Field
+                        name="description"
+                        component={this.renderInput}
+                        label="Enter Description"
+                    />
+                    <button className="ui button primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        // Errors should have the same name after . as the Field Component. Otherwise redux-form won't be able to correlated both things – errors with their respective field
+        errors.title = "You must enter a title";
+    }
+
+    if (!formValues.description) {
+        errors.description = "You must enter a description";
+    }
+
+    return errors;
+};
+
+export default reduxForm({
+    form: "streamForm",
+    validate,
+})(StreamForm);
