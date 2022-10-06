ba783bfb9d37141f9908529599872d6074d3c8cc
Gustavo_H <gustavo.hilario@automattic.com>
2022-10-06 13:07:48 -0500

    373. Creating a Portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Redirect users elsewhere when they click out of the modal
+import history from "../history";
+
 const Modal = (props) => {
+    // Modals are used to move React compontents/JSX to somewhere on our application. In this case, we are moving the component to be child of body so the modal could work properly
+
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standar modal visible active">TEST</div>
+        <div
+            className="ui dimmer modals visible active"
+            onClick={() => {
+                history.push("/");
+            }}
+        >
+            <div
+                className="ui standar modal visible active"
+                onClick={
+                    // Fixing EVENT BUBBLING
+                    (e) => e.stopPropagation()
+                }
+            >
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui button primary">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
+            </div>
         </div>,
         document.getElementById("modal")
     );
