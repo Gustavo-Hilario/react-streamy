3754b5ccb0c888d4f5104301a0e858c38c0bc672
Gustavo_H <gustavo.hilario@automattic.com>
2022-10-10 22:14:12 -0500

    391. Video Player Setup

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -3,7 +3,15 @@ import React from "react";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
 
+import flv from "flv.js";
+
 class StreamList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
         const streamId = this.props.match.params.id;
         // console.log(streamId);
@@ -23,6 +31,7 @@ class StreamList extends React.Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: "100%" }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
